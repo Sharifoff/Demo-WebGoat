a2b22b4860bfc9c48d31c9289b0e48303fa7c760
Sharifoff <efqanserifov@gmail.com>
2023-10-26 17:52:20 +0530

    Update Catcher.java
    sdgfdsgzds

--- src/main/java/org/owasp/webgoat/Catcher.java
+++ src/main/java/org/owasp/webgoat/Catcher.java
@@ -12,15 +12,7 @@ import org.owasp.webgoat.session.WebSession;
 
 
 /***************************************************************************************************
- * 
- * 
- * This file is part of WebGoat, an Open Web Application Security Project utility. For details,
- * please see http://www.owasp.org/
- * 
- * Copyright (c) 2002 - 20014 Bruce Mayhew
- * 
- * This program is free software; you can redistribute it and/or modify it under the terms of the
- * GNU General Public License as published by the Free Software Foundation; either version 2 of the
+ *ublic License as published by the Free Software Foundation; either version 2 of the
  * License, or (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
